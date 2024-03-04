765474d700025c602431187d3c60b6b1caaded5b
ElhamMoharrami <elham.moharrami.em@gmail.com>
2024-03-04 16:07:14 +0330

    add multi module structure

---
+++ transactionGenerator/src/main/java/com/generator/transactiongenerator/TransactionGeneratorApplication.java
@@ -0,0 +1,15 @@
+package com.generator.transactiongenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionGeneratorApplication {
+
+	public static void main(String[] args) {
+
+		//SpringApplication.run(TransactionGeneratorApplication.class, args);
+		System.out.println("hello from transaction generator");
+	}
+
+}
